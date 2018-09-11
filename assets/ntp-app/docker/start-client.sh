b92e6ecb1b5971f934a2d6c2982a2e59aeb7ae97
Roman Tkachenko <roman@gravitational.com>
2018-09-11 12:37:29 -0700

    Remove obsolete assets.

--- assets/ntp-app/docker/start-client.sh
+++
@@ -1,8 +0,0 @@
-while ! nslookup ntp; do
-    echo "Failed to resolve service name"
-    sleep 1
-done
-
-echo "Successfully resolved service name, starting ntpd"
-
-/usr/sbin/ntpd -n -c /etc/ntp/client.conf
