ec05a6eacae50a411f9574e0cac2485257c2a120
a-palchikov <deemok@gmail.com>
2018-10-09 08:32:15 +0200

    Dmitri/3754 gc (#42)
    * Allow garbage collection when the cluster is degraded
    
    * lib/vacuum/prune/pack: build package state prior to pruning to be able
    to delete in proper order.
    
    * Remove runtime application packages from previous versions.
    
    * Do not keep state in cleaner instance.
    Fix wording.
    
    * Address review comments
    
    * Rename depender -> owner package for clarity

--- lib/ops/opsservice/operationgroup.go
+++ lib/ops/opsservice/operationgroup.go
@@ -114,8 +114,9 @@ func (g *operationGroup) canCreateOperation(operation ops.SiteOperation) error {
 		if err != nil {
 			return trace.Wrap(err)
 		}
-	case ops.OperationShrink:
-		// shrink is allowed for degraded clusters (to be able to remove offline nodes)
+	case ops.OperationShrink, ops.OperationGarbageCollect:
+		// shrink and gc are allowed for degraded clusters
+		// shrink is allowed to be able to remove failed/offline nodes
 		switch cluster.State {
 		case ops.SiteStateActive, ops.SiteStateDegraded:
 		default:
