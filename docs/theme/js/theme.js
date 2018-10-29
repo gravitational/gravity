2237c9cb055c2a398d08bc26865afd5601e19ff5
Alex Wolfe <thewolfe@gmail.com>
2018-10-29 16:04:06 -0700

    search page

--- docs/theme/js/theme.js
+++ docs/theme/js/theme.js
@@ -98,32 +98,3 @@ $( document ).ready(function() {
     $('table').addClass('docutils');
 });
 
-window.SphinxRtdTheme = (function (jquery) {
-    var stickyNav = (function () {
-        var navBar,
-            win,
-            stickyNavCssClass = 'stickynav',
-            applyStickNav = function () {
-                if (navBar.height() <= win.height()) {
-                    navBar.addClass(stickyNavCssClass);
-                } else {
-                    navBar.removeClass(stickyNavCssClass);
-                }
-            },
-            enable = function () {
-                applyStickNav();
-                win.on('resize', applyStickNav);
-            },
-            init = function () {
-                navBar = jquery('nav.wy-nav-side:first');
-                win    = jquery(window);
-            };
-        jquery(init);
-        return {
-            enable : enable
-        };
-    }());
-    return {
-        StickyNav : stickyNav
-    };
-}($));
