9462677d4d64a26f42f5a913d8fe5289e8d431f6
dmitri <deemok@gmail.com>
2018-11-08 12:57:19 +0100

    Simplify robotest script to only run a minimum of tests. Set ground for extensive nightly tests

--- build.assets/robotest_run_suite.sh
+++ build.assets/robotest_run_suite.sh
@@ -3,9 +3,13 @@ set -eu -o pipefail
 
 readonly UPGRADE_FROM_DIR=${1:-$(pwd)/../upgrade_from}
 
-declare -A UPGRADE_FROM
+DOCKER_STORAGE_DRIVERS="overlay2 devicemapper"
+
+declare -A UPGRADE_MAP
+declare -A UPGRADE_NIGHTLY_MAP
 # gravity version -> list of OS releases to exercise on
-UPGRADE_FROM[5.0.24]="centos:7.5 ubuntu:latest"
+UPGRADE_MAP[5.0.24]="ubuntu:16"
+UPGRADE_NIGHTLY_MAP[5.0.24]="redhat:7 centos:7 debian:9 suse:12 ubuntu:16"
 
 readonly GET_GRAVITATIONAL_IO_APIKEY=${GET_GRAVITATIONAL_IO_APIKEY:?API key for distribution Ops Center required}
 readonly GRAVITY_BUILDDIR=${GRAVITY_BUILDDIR:?Set GRAVITY_BUILDDIR to the build directory}
@@ -22,6 +26,13 @@ export TAG=$(git rev-parse --short HEAD)
 export GCL_PROJECT_ID=${GCL_PROJECT_ID:-"kubeadm-167321"}
 export AZURE_REGION=${AZURE_REGION:-"westus,westus2,centralus,canadacentral"}
 
+function build_nightly_resize_suite {
+  cat <<EOF
+ resize={"to":3,"flavor":"one","nodes":1,"role":"node","state_dir":"/var/lib/telekube","os":"ubuntu:latest","storage_driver":"devicemapper"}
+ resize={"to":6,"flavor":"three","nodes":3,"role":"node","state_dir":"/var/lib/telekube","os":"ubuntu:latest","storage_driver":"overlay2"}
+EOF
+}
+
 function build_resize_suite {
   cat <<EOF
  resize={"to":3,"flavor":"one","nodes":1,"role":"node","state_dir":"/var/lib/telekube","os":"ubuntu:latest","storage_driver":"devicemapper"}
@@ -29,69 +40,93 @@ EOF
 }
 
 function build_upgrade_step {
-  local usage="$FUNCNAME os release"
+  local usage="$FUNCNAME os release storage-driver cluster-size"
   local os=${1:?$usage}
   local release=${2:?$usage}
-  local cluster_sizes=('"flavor":"three","nodes":3,"role":"node"' '"flavor":"six","nodes":6,"role":"node"' '"flavor":"one","nodes":1,"role":"node"')
+  local storage_driver=${3:?$usage}
   local suite=''
-  for size in ${cluster_sizes[@]}; do
-    suite+=$(cat <<EOF
- upgrade3lts={${size},"os":"${os}","storage_driver":"overlay2","from":"/telekube_${release}.tar"}
+  suite+=$(cat <<EOF
+ upgrade3lts={${cluster_size},"os":"${os}","storage_driver":"${storage_driver}","from":"/telekube_${release}.tar"}
 EOF
 )
-  done
   echo $suite
 }
 
-function build_devicemapper_upgrade_step {
-  local usage="$FUNCNAME os release"
-  local os=${1:?$usage}
-  local release=${2:?$usage}
+function build_nightly_upgrade_suite {
   local suite=''
-  suite+=$(cat <<EOF
- upgrade3lts={"flavor":"three","nodes":3,"role":"node","os":"${os}","storage_driver":"devicemapper","from":"/telekube_${release}.tar"}
-EOF
-)
+  local cluster_sizes=( \
+    '"flavor":"three","nodes":3,"role":"node"' \
+    '"flavor":"six","nodes":6,"role":"node"' \
+    '"flavor":"one","nodes":1,"role":"node"')
+  for release in ${!UPGRADE_NIGHTLY_MAP[@]}; do
+    for os in ${UPGRADE_NIGHTLY_MAP[$release]}; do
+      for storage_driver in ${DOCKER_STORAGE_DRIVERS[@]}; do
+        for size in ${cluster_sizes[@]}; do
+          suite+=$(build_upgrade_step $os $release $storage_driver $size)
+          suite+=' '
+        done
+      done
+    done
+  done
   echo $suite
 }
 
 function build_upgrade_suite {
   local suite=''
-  for release in ${!UPGRADE_FROM[@]}; do
-    for os in ${UPGRADE_FROM[$release]}; do
-      suite+=$(build_upgrade_step $os $release)
+  local cluster_size='"flavor":"six","nodes":6,"role":"node"'
+  for release in ${!UPGRADE_MAP[@]}; do
+    for os in ${UPGRADE_MAP[$release]}; do
+      suite+=$(build_upgrade_step $os $release 'overlay2' $cluster_size)
       suite+=' '
     done
-    suite+=$(build_devicemapper_upgrade_step 'redhat:7.4' $release)
-    suite+=' '
   done
+  echo $suite
+}
+
+function build_ops_install_suite {
+  local suite=$(cat <<EOF
+ install={"installer_url":"/installer/opscenter.tar","nodes":1,"flavor":"standalone","role":"node","os":"ubuntu:16","ops_advertise_addr":"example.com:443"}
+EOF
+)
+  echo $suite
+}
+
+function build_nightly_install_suite {
+  local suite=''
+  local test_os="redhat:7 centos:7 suse:12 debian:9 ubuntu:16"
+  local cluster_sizes=( \
+    '"flavor":"three","nodes":3,"role":"node"' \
+    '"flavor":"six","nodes":6,"role":"node"')
+  for os in $test_os; do
+    for storage_driver in ${DOCKER_STORAGE_DRIVERS[@]}; do
+      for size in ${cluster_sizes[@]}; do
+        suite+=$(cat <<EOF
+ install={${size},"os":"${os}","storage_driver":"${storage_driver}"}
+EOF
+        suite+=' '
+)
+      done
+    done
+  done
+  suite+=$(build_ops_install_suite)
   echo $suite
 }
 
 function build_install_suite {
   local suite=''
-  local test_os="redhat:7.5 centos:7.5 ubuntu:latest"
-  local cluster_sizes=('"flavor":"three","nodes":3,"role":"node"' '"flavor":"six","nodes":6,"role":"node"')
-  local storage_drivers="overlay2 devicemapper"
-  for os in $test_os; do
-    for driver in $storage_drivers; do
-      for size in ${cluster_sizes[@]}; do
-        suite+=$(cat <<EOF
- install={${size},"os":"${os}","storage_driver":"${driver}"}
-EOF
-)
-      done
-    done
-  done
+  local test_os="redhat:7"
+  local cluster_size='"flavor":"six","nodes":6,"role":"node"'
   suite+=$(cat <<EOF
- install={"installer_url":"/installer/opscenter.tar","nodes":1,"flavor":"standalone","role":"node","os":"ubuntu:latest","ops_advertise_addr":"example.com:443"}
+ install={${cluster_size},"os":"${test_os}","storage_driver":"overlay2"}
 EOF
 )
+  suite+=' '
+  suite+=$(build_ops_install_suite)
   echo $suite
 }
 
 function build_volume_mounts {
-  for release in ${!UPGRADE_FROM[@]}; do
+  for release in ${!UPGRADE_MAP[@]}; do
     echo "-v $UPGRADE_FROM_DIR/telekube_${release}.tar:/telekube_${release}.tar"
   done
 }
@@ -106,7 +141,7 @@ echo $suite
 
 mkdir -p $UPGRADE_FROM_DIR
 tele login --ops=https://get.gravitational.io:443 --key="$GET_GRAVITATIONAL_IO_APIKEY"
-for release in ${!UPGRADE_FROM[@]}; do
+for release in ${!UPGRADE_MAP[@]}; do
   tele pull telekube:$release --output=$UPGRADE_FROM_DIR/telekube_$release.tar
 done
 
