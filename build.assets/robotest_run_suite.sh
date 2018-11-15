24549f4bf4ee44c01b73edfe10526b5bf1531acb
Roman Tkachenko <roman@gravitational.com>
2018-11-15 15:36:59 -0800

    Change 'upgrade from' release for robotest to 5.2.3.

--- build.assets/robotest_run_suite.sh
+++ build.assets/robotest_run_suite.sh
@@ -5,7 +5,7 @@ readonly UPGRADE_FROM_DIR=${1:-$(pwd)/../upgrade_from}
 
 declare -A UPGRADE_FROM
 # gravity version -> list of OS releases to exercise on
-UPGRADE_FROM[5.0.24]="centos:7.5 ubuntu:latest"
+UPGRADE_FROM[5.2.3]="centos:7.5 ubuntu:latest"
 
 readonly GET_GRAVITATIONAL_IO_APIKEY=${GET_GRAVITATIONAL_IO_APIKEY:?API key for distribution Ops Center required}
 readonly GRAVITY_BUILDDIR=${GRAVITY_BUILDDIR:?Set GRAVITY_BUILDDIR to the build directory}
