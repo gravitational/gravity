0360dbd2a2cf33214ecfd0e8ef8867b9eee6155c
dmitri <deemok@gmail.com>
2019-06-04 20:18:57 +0200

    Address review comments

--- lib/ops/opsservice/configure.go
+++ lib/ops/opsservice/configure.go
@@ -924,7 +924,7 @@ func (s *site) getPlanetConfig(config planetConfig) (args []string, err error) {
 	}
 
 	for k, v := range config.env {
-		args = append(args, fmt.Sprintf("--env=%v=%v", k, maybeEnvValueQuoted(v)))
+		args = append(args, fmt.Sprintf("--env=%v=%v", k, strconv.Quote(v)))
 	}
 
 	args = append(args, s.addCloudConfig(config.config)...)
@@ -1593,13 +1593,6 @@ func configureDockerOptions(
 	}
 
 	return args, nil
-}
-
-func maybeEnvValueQuoted(value string) string {
-	if strings.ContainsAny(value, `,;:"' =`) {
-		return strconv.Quote(value)
-	}
-	return value
 }
 
 // exportBackend defines a shim to export site information
