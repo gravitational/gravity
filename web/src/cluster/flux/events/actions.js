33167b147f2b3dace2a4de95e67b9aa19d37c0d2
Alexey Kontsevoy <biz.kovoy@gmail.com>
2019-07-12 14:28:26 -0400

    Use the maximum allowed limit to fetch audit events

--- web/src/cluster/flux/events/actions.js
+++ web/src/cluster/flux/events/actions.js
@@ -19,27 +19,27 @@ import reactor from 'app/reactor';
 import { fetchClusterEvents } from 'app/cluster/services/events';
 import { CLUSTER_EVENTS_RECEIVE, CLUSTER_EVENTS_RECEIVE_LATEST } from './actionTypes';
 
-export const EVENT_MAX_LIMIT = 500;
+export const EVENT_MAX_LIMIT = 9999;
 
-export function fetchLatest(siteId){
+export function fetchLatest(siteId) {
   const start = moment(new Date()).startOf('day').toDate().toISOString();
   const end = moment(new Date()).endOf('day').toDate().toISOString();
-  return fetchClusterEvents({ siteId, start, end  })
+  return fetchClusterEvents({ siteId, start, end })
     .done(events => {
       reactor.dispatch(CLUSTER_EVENTS_RECEIVE_LATEST, events);
     });
 }
 
 // Fetchs events within given date range and detects if
-// number of events exceeds the maximum allowed limit
-export function fetchEvents({siteId, end, start}){
+// number of events exceeds the maximum limit allowed
+export function fetchEvents({ siteId, end, start }) {
   start = start.toISOString();
   end = end.toISOString();
 
   return fetchClusterEvents({ siteId, start, end, limit: EVENT_MAX_LIMIT + 1 })
     .done(events => {
       const overflow = events.length > EVENT_MAX_LIMIT;
-      events = events.splice(0, EVENT_MAX_LIMIT - 1)
+      events = events.splice(0, EVENT_MAX_LIMIT - 1);
       reactor.dispatch(CLUSTER_EVENTS_RECEIVE, { overflow, events });
     });
 }
