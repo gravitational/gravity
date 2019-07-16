db30616353aee88356f3f4d22fb764021b1b0253
Roman Tkachenko <roman@gravitational.com>
2019-07-16 16:42:59 +0300

    Address review comments.

--- tool/tele/cli/register.go
+++ tool/tele/cli/register.go
@@ -43,7 +43,7 @@ func RegisterCommands(app *kingpin.Application) Application {
 	tele.BuildCmd.CmdClause = app.Command("build", "Cluster and application image build tool.")
 	tele.BuildCmd.ManifestPath = tele.BuildCmd.Arg("path", fmt.Sprintf("Path to the cluster image manifest file (must be named %q), or unpacked Helm chart to build an application image out of.", defaults.ManifestFileName)).Default(defaults.ManifestFileName).String()
 	tele.BuildCmd.OutFile = tele.BuildCmd.Flag("output", "Cluster or application image file name. Defaults to <name>-<version>.tar.").Short('o').String()
-	tele.BuildCmd.Overwrite = tele.BuildCmd.Flag("overwrite", "Overwrite the existing image file if it already exists.").Short('f').Bool()
+	tele.BuildCmd.Overwrite = tele.BuildCmd.Flag("overwrite", "Overwrite the existing image file.").Short('f').Bool()
 	tele.BuildCmd.Repository = tele.BuildCmd.Flag("repository", "Optional address of Gravity Hub to download dependencies from.").Hidden().String()
 	tele.BuildCmd.Name = tele.BuildCmd.Flag("name", "Optional cluster image name, overrides the one specified in the manifest file.").Hidden().String()
 	tele.BuildCmd.Version = tele.BuildCmd.Flag("version", "Optional cluster image version, overrides the one specified in the manifest file.").Hidden().String()
@@ -53,9 +53,9 @@ func RegisterCommands(app *kingpin.Application) Application {
 	tele.BuildCmd.SetDeps = loc.LocatorSlice(tele.BuildCmd.Flag("set-dep", "Rewrite dependencies section in the image manifest file during vendoring, e.g. 'gravitational.io/site-app:0.0.39' will overwrite dependency to 'gravitational.io/site-app:0.0.39'.").Hidden())
 	tele.BuildCmd.SkipVersionCheck = tele.BuildCmd.Flag("skip-version-check", "Skip version compatibility check.").Hidden().Bool()
 	tele.BuildCmd.Parallel = tele.BuildCmd.Flag("parallel", "Specifies the number of concurrent tasks. If < 0, the number of tasks is not restricted, if unspecified, then tasks are capped at the number of logical CPU cores.").Int()
-	tele.BuildCmd.Quiet = tele.BuildCmd.Flag("quiet", "Suppress any extra output to stdout.").Short('q').Bool()
+	tele.BuildCmd.Quiet = tele.BuildCmd.Flag("quiet", "Suppress any output to stdout.").Short('q').Bool()
 
-	tele.ListCmd.CmdClause = app.Command("ls", "Display a list of cluster and application images published to Gravity Hub.")
+	tele.ListCmd.CmdClause = app.Command("ls", "List cluster and application images published to Gravity Hub.")
 	tele.ListCmd.Runtimes = tele.ListCmd.Flag("runtimes", "Show only runtimes.").Short('r').Hidden().Bool()
 	tele.ListCmd.Format = common.Format(tele.ListCmd.Flag("format", fmt.Sprintf("Output format: %v.", constants.OutputFormats)).Default(string(constants.EncodingText)))
 	tele.ListCmd.All = tele.ListCmd.Flag("all", "Display all available versions.").Bool()
@@ -63,8 +63,8 @@ func RegisterCommands(app *kingpin.Application) Application {
 	tele.PullCmd.CmdClause = app.Command("pull", "Pull a cluster or application image from Gravity Hub.")
 	tele.PullCmd.App = tele.PullCmd.Arg("image", "Cluster or application image to download: <name>:<version> or just <name> to download the latest.").Required().String()
 	tele.PullCmd.OutFile = tele.PullCmd.Flag("output", "File name of the downloaded image. Defaults to <name>-<version>.tar.").Short('o').String()
-	tele.PullCmd.Force = tele.PullCmd.Flag("force", "Overwrite the existing image file if it already exists.").Short('f').Bool()
-	tele.PullCmd.Quiet = tele.PullCmd.Flag("quiet", "Suppress any extra output to stdout.").Short('q').Bool()
+	tele.PullCmd.Force = tele.PullCmd.Flag("force", "Overwrite the existing image file.").Short('f').Bool()
+	tele.PullCmd.Quiet = tele.PullCmd.Flag("quiet", "Suppress any output to stdout.").Short('q').Bool()
 
 	return tele
 }
