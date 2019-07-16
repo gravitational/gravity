4da3be1c3511c336522d749b859c7575c4dacdde
a-palchikov <deemok@gmail.com>
2019-07-16 12:34:13 +0200

    Unpack application resources without the 'resources' directory (#501)
    * In #452, if the application resource directory itself contains
    a 'resources' subfolder, hook init containers fail.
    Change the semantics of 'app unpack' to unpack application resources
    from the tarball's 'resources' directory directly into the specified
    directory without the 'resources' directory itself.
    
    Fixes #452.

--- lib/rpc/server/suite_test.go
+++ lib/rpc/server/suite_test.go
@@ -18,7 +18,6 @@ package server
 
 import (
 	"net"
-	"os"
 	"testing"
 
 	"github.com/gravitational/trace"
@@ -33,13 +32,6 @@ type S struct {
 }
 
 var _ = check.Suite(&S{})
-
-func init() {
-	if testing.Verbose() {
-		log.SetLevel(log.DebugLevel)
-		log.SetOutput(os.Stderr)
-	}
-}
 
 func (r *S) SetUpSuite(c *check.C) {
 	r.Logger = log.StandardLogger()
