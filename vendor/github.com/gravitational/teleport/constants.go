4be5552d3a6593177b72f5c5b2622b74c8565fe8
Kevin Nisbet <kevin@gravitational.com>
2019-09-16 23:37:16 +0000

    revendor teleport with fixes for doyensec security findings

--- vendor/github.com/gravitational/teleport/constants.go
+++ vendor/github.com/gravitational/teleport/constants.go
@@ -324,6 +324,8 @@ const (
 	CertExtensionPermitPortForwarding = "permit-port-forwarding"
 	// CertExtensionTeleportRoles is used to propagate teleport roles
 	CertExtensionTeleportRoles = "teleport-roles"
+	// CertExtensionTeleportTraits is used to propagate traits about the user.
+	CertExtensionTeleportTraits = "teleport-traits"
 )
 
 const (
@@ -433,6 +435,10 @@ const (
 	// SessionEvent is sent by servers to clients when an audit event occurs on
 	// the session.
 	SessionEvent = "x-teleport-event"
+
+	// VersionRequest is sent by clients to server requesting the Teleport
+	// version they are running.
+	VersionRequest = "x-teleport-version"
 )
 
 const (
