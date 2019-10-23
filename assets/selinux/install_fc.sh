c03e9eae3b9fc0fc998062fc3209c690bd195da2
dmitri <deemok@gmail.com>
2019-10-23 22:45:25 +0200

    Refactor policy (use better names, group permissions, introduce more container configuration. Update go-ps and logrus

--- assets/selinux/install_fc.sh
+++ assets/selinux/install_fc.sh
@@ -9,51 +9,74 @@ DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 function setup_file_contexts {
   # Label the current directory for installer
-  semanage fcontext -a -t gravity_installer_t "${DIR}(/.*)?"
+  semanage fcontext -a -t gravity_installer_home_t "${DIR}(/.*)?"
   # Label the installer
-  semanage fcontext -a -t gravity_exec_t "${DIR}/gravity"
-  semanage fcontext -a -t gravity_log_t "${DIR}/gravity-(install|system)\.log"
-  semanage fcontext -a -t gravity_state_t "${DIR}/.gravity"
-  semanage fcontext -a -t gravity_unit_t "${DIR}/.gravity/gravity-(installer|agent)\.service"
-  semanage fcontext -a -t gravity_state_t "${DIR}/crashreport(.*)?\.tgz"
+  semanage fcontext -a -t gravity_exec_t -f f "${DIR}/gravity"
+  semanage fcontext -a -t gravity_log_t -f f "${DIR}/gravity-(install|system)\.log"
+  semanage fcontext -a -t gravity_home_t "${DIR}/.gravity"
+  semanage fcontext -a -t gravity_unit_file_t -f f "${DIR}/.gravity/gravity-(installer|agent)\.service"
+  semanage fcontext -a -t gravity_home_t -f f "${DIR}/crashreport(.*)?\.tgz"
   # Apply labels
+  restorecon -Rv "${DIR}"
+}
+
+function restore_file_contexts {
+  semanage fcontext -a -t user_home_t "${DIR}(/.*)?"
   restorecon -Rv "${DIR}"
 }
 
 function setup_ports {
   # https://danwalsh.livejournal.com/10607.html
   # Installer-specific ports
-  semanage port -m -t gravity_installer_port_t -p tcp 61009-61010
-  semanage port -m -t gravity_installer_port_t -p tcp 61022-61025
-  semanage port -m -t gravity_installer_port_t -p tcp 61080
+  semanage port -a -t gravity_installer_port_t -p tcp 61009-61010
+  semanage port -a -t gravity_installer_port_t -p tcp 61022-61025
+  semanage port -a -t gravity_installer_port_t -p tcp 61080
   # Cluster ports
   # Gravity RPC agent
-  semanage port -m -t gravity_agent_port_t -p tcp 3012
-  semanage port -m -t gravity_agent_port_t -p tcp 7575
+  semanage port -a -t gravity_agent_port_t -p tcp 3012
+  semanage port -a -t gravity_agent_port_t -p tcp 7575
   # Gravity Hub control panel
-  semanage port -m -t gravity_port_t -p tcp 32009
+  semanage port -a -t gravity_port_t -p tcp 32009
   # Gravity (teleport internal SSH control plane)
-  semanage port -m -t gravity_port_t -p tcp 3022-3025
+  semanage port -a -t gravity_port_t -p tcp 3022-3025
   # Gravity (teleport web UI)
-  semanage port -m -t gravity_port_t -p tcp 3080
+  semanage port -a -t gravity_port_t -p tcp 3080
   # Gravity (internal gravity services)
-  semanage port -m -t gravity_port_t -p tcp 3008-3011
+  semanage port -a -t gravity_port_t -p tcp 3008-3011
   # Gravity (vxlan)
-  semanage port -m -t gravity_vxlan_port_t -p tcp 8472
+  semanage port -a -t gravity_vxlan_port_t -p tcp 8472
   # serf peer-to-peer
-  semanage port -m -t gravity_kubernetes_port_t -p tcp 7373
-  semanage port -m -t gravity_kubernetes_port_t -p tcp 7496
+  semanage port -a -t gravity_kubernetes_port_t -p tcp 7373
+  semanage port -a -t gravity_kubernetes_port_t -p tcp 7496
   # reserved and overridden in the policy
   # semanage port -a -t gravity_docker_port_t -p tcp 5000
   # Kubernetes (etcd)
-  semanage port -m -t gravity_kubernetes_port_t -p tcp 2379-2380
+  semanage port -a -t gravity_kubernetes_port_t -p tcp 2379-2380
   # reserved and overridden in the policy
   # semanage port -a -t gravity_kubernetes_port_t -p tcp 4001
   # semanage port -a -t gravity_kubernetes_port_t -p tcp 7001
   # Kubernetes (apiserver)
   # semanage port -a -t gravity_kubernetes_port_t -p tcp 6443
   # Kubernetes (kubelet)
-  semanage port -m -t gravity_kubernetes_port_t -p tcp 10248-10255
+  semanage port -a -t gravity_kubernetes_port_t -p tcp 10248-10255
+}
+
+function remove_ports {
+  semanage port -d -t gravity_installer_port_t -p tcp 61009-61010
+  semanage port -d -p tcp 61022-61025
+  semanage port -d -p tcp 61080
+  semanage port -d -p tcp 3012
+  semanage port -d -p tcp 7575
+  semanage port -d -p tcp 32009
+  semanage port -d -p tcp 3022-3025
+  semanage port -d -p tcp 3080
+  semanage port -d -p tcp 3008-3011
+  semanage port -d -p tcp 8472
+  semanage port -d -p tcp 7373
+  semanage port -d -p tcp 7496
+  semanage port -d -p tcp 6443
+  semanage port -d -p tcp 2379-2380
+  semanage port -d -p tcp 10248-10255
 }
 
 setup_file_contexts
--- lib/update/system/system.go
+++ lib/update/system/system.go
@@ -38,6 +38,7 @@ import (
 
 	"github.com/docker/docker/pkg/archive"
 	"github.com/gravitational/trace"
+	"github.com/opencontainers/selinux/go-selinux"
 	"github.com/sirupsen/logrus"
 )
 
@@ -341,6 +342,11 @@ func (r *PackageUpdater) updatePlanetPackage(update storage.PackageUpdate) (labe
 	}
 
 	planetPath, err := r.Packages.UnpackedPath(update.To)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	err = r.applySelinuxFilecontexts(planetPath)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -732,4 +738,17 @@ func unpack(packages update.LocalPackageService, loc loc.Locator) error {
 		return trace.Wrap(err)
 	}
 	return packages.Unpack(loc, path)
+}
+
+func (r *System) applySelinuxFilecontexts(path string) error {
+	if !selinux.GetEnabled() {
+		return nil
+	}
+	out, err := exec.Command("restorecon", "-R", "-v", path).CombinedOutput()
+	if err != nil {
+		r.WithError(err).Warn("Failed to restorecon file contexts.")
+		return trace.Wrap(err, "failed to restorecon file contexts on %v: %s",
+			path, string(out))
+	}
+	return nil
 }
