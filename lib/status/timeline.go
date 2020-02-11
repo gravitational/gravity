fee9a264fa0c059413c6ef1c5e29b3359eef80ee
Bernard Kim <bernard@gravitational.com>
2020-02-11 12:16:41 -0800

    CR fix
    - `7.0.5` -> `=7.0.5`
    - Add `GetGRPCPlanetClient` func

--- lib/status/timeline.go
+++ lib/status/timeline.go
@@ -18,41 +18,18 @@ package status
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/gravitational/gravity/lib/constants"
-	"github.com/gravitational/gravity/lib/defaults"
-	"github.com/gravitational/gravity/lib/state"
-	"github.com/gravitational/gravity/lib/utils"
+	"github.com/gravitational/gravity/lib/httplib"
 
 	pb "github.com/gravitational/satellite/agent/proto/agentpb"
-	"github.com/gravitational/satellite/lib/rpc/client"
 	"github.com/gravitational/trace"
 )
 
 // Timeline queries the currently stored cluster timeline.
 func Timeline(ctx context.Context) (*pb.TimelineResponse, error) {
-	addr := fmt.Sprintf("%v:%v", constants.Localhost, defaults.SatelliteRPCAgentPort)
-
-	stateDir, err := state.GetStateDir()
+	client, err := httplib.GetGRPCPlanetClient(ctx)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-
-	caFile := state.Secret(stateDir, defaults.RootCertFilename)
-	clientCertFile := state.Secret(stateDir, fmt.Sprint(constants.PlanetRpcKeyPair, ".", utils.CertSuffix))
-	clientKeyFile := state.Secret(stateDir, fmt.Sprint(constants.PlanetRpcKeyPair, ".", utils.KeySuffix))
-
-	config := client.Config{
-		Address:  addr,
-		CAFile:   caFile,
-		CertFile: clientCertFile,
-		KeyFile:  clientKeyFile,
-	}
-	client, err := client.NewClient(ctx, config)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
 	return client.Timeline(ctx, &pb.TimelineRequest{})
 }
--- tool/gravity/cli/register.go
+++ tool/gravity/cli/register.go
@@ -213,8 +213,10 @@ func RegisterCommands(app *kingpin.Application) *Application {
 	g.UpdateSystemCmd.WithStatus = g.UpdateSystemCmd.Flag("with-status", "Verify the system status at the end of the operation").Bool()
 	g.UpdateSystemCmd.RuntimePackage = Locator(g.UpdateSystemCmd.Flag("runtime-package", "The name of the runtime package to update to").Required())
 
+	// Display cluster status information
 	g.StatusCmd.CmdClause = g.Command("status", "Display overall cluster status.")
 
+	// Display current overall cluster status
 	g.StatusClusterCmd.CmdClause = g.StatusCmd.Command("cluster", "Display overall cluster status.").Default()
 	g.StatusClusterCmd.Token = g.StatusClusterCmd.Flag("token", "Display only the cluster join token.").Bool()
 	g.StatusClusterCmd.Tail = g.StatusClusterCmd.Flag("tail", "Tail logs of the currently running operation until it completes.").Bool()
@@ -222,7 +224,7 @@ func RegisterCommands(app *kingpin.Application) *Application {
 	g.StatusClusterCmd.Seconds = g.StatusClusterCmd.Flag("seconds", "Continuously display status every N seconds.").Short('s').Int()
 	g.StatusClusterCmd.Output = common.Format(g.StatusClusterCmd.Flag("output", "Output format: json or text.").Default(string(constants.EncodingText)))
 
-	// display cluster status history
+	// Display cluster status history
 	g.StatusHistoryCmd.CmdClause = g.StatusCmd.Command("history", "Display cluster status history.")
 
 	// reset cluster state, for debugging/emergencies
