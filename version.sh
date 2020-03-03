94cc8ec835ab0c379dddef376c105c00fc2f8c2c
Walt Della <walt@gravitational.com>
2020-03-02 16:56:48 -0800

    Add 'dev' signifier to post-release versions.
    In order to help distinguish development builds, add 'dev' to the
    pre-release portion of the semantic version string.
    
    (cherry picked from commit ddadb12164be9ecc6aa5f2725644430c908be240)

--- version.sh
+++ version.sh
@@ -2,8 +2,12 @@
 
 # this versioning algo:
 #  - if on a tagged commit, use the tag
-#  - if last tag was a regular relase, bump the minor version, make a it pre-release, and append # of commits since tag
-#  - if last tag was a pre-release tag, append number of commits since the tag
+#    e.g. 6.2.18 (for the commit tagged 6.2.18)
+#  - if last tag was a regular release, bump the minor version, make a it a 'dev' pre-release, and append # of commits since tag
+#    e.g. 5.5.38-dev.5 (for 5 commits after 5.5.37)
+#  - if last tag was a pre-release tag (e.g. alpha, beta, rc), append number of commits since the tag
+#    e.g. 7.0.0-alpha.1.5 (for 5 commits after 7.0.0-alpha.1)
+
 
 increment_patch() {
     # increment_patch returns x.y.(z+1) given valid x.y.z semver.
@@ -26,7 +30,7 @@ if [[ "$LONG_TAG" == "$SHORT_TAG" ]] ; then  # the current commit is tagged as a
     echo "$SHORT_TAG"
 elif [[ "$SHORT_TAG" != *-* ]] ; then  # the current ref is not a decendent of a pre-release version
     SHORT_TAG=$(increment_patch ${SHORT_TAG})
-    echo "$SHORT_TAG-${COMMITS_SINCE_LAST_TAG}"
+    echo "$SHORT_TAG-dev.${COMMITS_SINCE_LAST_TAG}"
 else  # the current ref is a decendent of a pre-release version (e.g. already an rc, alpha, or beta)
     echo "$SHORT_TAG.${COMMITS_SINCE_LAST_TAG}"
 fi
