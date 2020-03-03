a607cf5f00e38e82a37916946f8a9d947afb5ade
Walt Della <walt@gravitational.com>
2020-03-02 16:48:02 -0800

    Increment patch version for new content.
    Before this commit, consecutive commits on top of tag x.y.z would result
    in a version of x.y.z-c, where c was the number of commits since the
    tag.  This was not semver compatible, because a hyphen is used to denote
    a prerelease, as discussed at:
    
    https://semver.org/#spec-item-9
    
    Particularly: "Pre-release versions have a lower precedence than the
    associated normal version."
    
    After this patch, we increment the patch version before designating it
    as a pre-release. This leads to semver compatible versioning like:
    
    5.5.37 < 5.5.38-1 < 5.5.38
    
    (cherry picked from commit 298a14e7d411ad80d717d186a20cfe92fc54c565)

--- version.sh
+++ version.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 # this versioning algo:
-# keeps tag as is in case if this version is an equal match
-# otherwise adds .<number of commits since last tag>
+#  - if on a tagged commit, use the tag
+#  - if last tag was a regular relase, bump the minor version, make a it pre-release, and append # of commits since tag
+#  - if last tag was a pre-release tag, append number of commits since the tag
+
+increment_patch() {
+    # increment_patch returns x.y.(z+1) given valid x.y.z semver.
+    # If we need to robustly handle this, it is probably worth
+    # looking at https://github.com/davidaurelio/shell-semver/
+    # or moving this logic to a 'real' programming language -- 2020-03 walt
+    major=$(echo $1 | cut -d'.' -f1)
+    minor=$(echo $1 | cut -d'.' -f2)
+    patch=$(echo $1 | cut -d'.' -f3)
+    patch=$((patch + 1))
+    echo "${major}.${minor}.${patch}"
+}
 
 SHORT_TAG=`git describe --abbrev=0 --tags`
 LONG_TAG=`git describe --tags`
 COMMIT_WITH_LAST_TAG=`git show-ref --tags --dereference | grep "refs/tags/${SHORT_TAG}^{}" | awk '{print $1}'`
 COMMITS_SINCE_LAST_TAG=`git rev-list  ${COMMIT_WITH_LAST_TAG}..HEAD --count`
 
-if [[ "$LONG_TAG" == "$SHORT_TAG" ]] ; then
+if [[ "$LONG_TAG" == "$SHORT_TAG" ]] ; then  # the current commit is tagged as a release
     echo "$SHORT_TAG"
-elif [[ "$SHORT_TAG" != *-* ]] ; then
+elif [[ "$SHORT_TAG" != *-* ]] ; then  # the current ref is not a decendent of a pre-release version
+    SHORT_TAG=$(increment_patch ${SHORT_TAG})
     echo "$SHORT_TAG-${COMMITS_SINCE_LAST_TAG}"
-else
+else  # the current ref is a decendent of a pre-release version (e.g. already an rc, alpha, or beta)
     echo "$SHORT_TAG.${COMMITS_SINCE_LAST_TAG}"
 fi
