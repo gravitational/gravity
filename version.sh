291b703c84c35a9f26d5c7d27111f316d5a8fb57
Walt Della <walt@gravitational.com>
2020-03-02 16:45:32 -0800

    Simplify commit determination.
    Previously the code grepped through all tags, looking for a dereferenced
    version of $SHORT_TAG. This fails when $SHORT_TAG is not an annotated
    tag, but a lightweight tag (and thus has no dereference).
    
    `git show-rev` will always find the commit a tag points to, regardless
    of tag type, and is considerably shorter.

--- version.sh
+++ version.sh
@@ -6,7 +6,7 @@
 
 SHORT_TAG=`git describe --abbrev=0 --tags`
 LONG_TAG=`git describe --tags`
-COMMIT_WITH_LAST_TAG=`git show-ref --tags --dereference | grep "refs/tags/${SHORT_TAG}^{}" | awk '{print $1}'`
+COMMIT_WITH_LAST_TAG=`git rev-list -n1 ${SHORT_TAG}`
 COMMITS_SINCE_LAST_TAG=`git rev-list  ${COMMIT_WITH_LAST_TAG}..HEAD --count`
 
 if [[ "$LONG_TAG" == "$SHORT_TAG" ]] ; then
