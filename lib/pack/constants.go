4f398abfe37cc91789852e7e7089595d51f2deca
Roman Tkachenko <roman@gravitational.com>
2020-03-16 13:04:13 -0700

    (6.3) Add runtime upgrade path checks. (#1237)

--- lib/pack/constants.go
+++ lib/pack/constants.go
@@ -44,6 +44,8 @@ const (
 	PurposePlanetConfig = "planet-config"
 	// PurposeRuntime marks a package as a runtime container package
 	PurposeRuntime = "runtime"
+	// PurposeRuntimeUpgrade marks a package as part of an intermediate upgrade step
+	PurposeRuntimeUpgrade = "intermediate-upgrade"
 	// PurposeTeleportMasterConfig marks package with teleport master config
 	PurposeTeleportMasterConfig = "teleport-master-config"
 	// PurposeTeleportNodeConfig marks package with teleport node config
@@ -97,3 +99,11 @@ var (
 		InstalledLabel: InstalledLabel,
 	}
 )
+
+// RuntimeUpgradeLabels returns the labels to mark a package as part of an upgrade step
+// for the specified runtime version
+func RuntimeUpgradeLabels(version string) Labels {
+	return Labels{
+		PurposeRuntimeUpgrade: version,
+	}
+}
