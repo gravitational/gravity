9bcc20126083b48ca8c7858472a36ad027292fd0
Roman Tkachenko <roman@gravitational.com>
2020-04-03 14:07:37 -0700

    Preserve hub sni during join.

--- lib/ops/opsservice/configure.go
+++ lib/ops/opsservice/configure.go
@@ -340,7 +340,6 @@ func (s *site) configurePackages(ctx *operationContext, req ops.ConfigurePackage
 			master:            master,
 			secretsPackage:    &secretsPackage,
 			serviceSubnetCIDR: ctx.operation.InstallExpand.Subnets.Service,
-			sniHost:           s.service.cfg.SNIHost,
 		})
 		if err != nil && !trace.IsAlreadyExists(err) {
 			s.WithField("package", secretsPackage.String()).Info("Planet secrets package already exists.")
@@ -672,6 +671,11 @@ func (s *site) getPlanetMasterSecretsPackage(ctx *operationContext, p planetMast
 			}
 			// this will make APIServer's certificate valid for requests
 			// via OpsCenter, e.g. siteDomain.opscenter.example.com
+			if s.service.cfg.SNIHost != "" {
+				req.Hosts = append(req.Hosts, strings.Join([]string{
+					s.domainName, s.service.cfg.SNIHost}, "."))
+			}
+			// add additional hosts that have been passed in
 			if p.sniHost != "" {
 				req.Hosts = append(req.Hosts, strings.Join([]string{
 					s.domainName, p.sniHost}, "."))
