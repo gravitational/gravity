34145c31527eb7f7d8b7e781ea5a5ace2fb2c5b9
Bernard Kim <bernard@gravitational.com>
2020-04-06 15:18:29 -0700

    [7.0.x] Add nethealth checker (#1326)
    - Bump satellite to 7.0.8
    - Bump planet to 7.0.23
    - Bump monitoring-app to 6.0.7

--- vendor/github.com/gravitational/satellite/agent/constants.go
+++ vendor/github.com/gravitational/satellite/agent/constants.go
@@ -40,10 +40,10 @@ const (
 
 // Timeout values
 const (
-	// lastSeenTTL specifies the time to live for the stored lastSeen values.
+	// lastSeenTTLSeconds specifies the time to live for the stored lastSeen values.
 	// This ensures agents do not hold on to unused information when a member
 	// leaves the cluster.
-	lastSeenTTL = 3 * time.Minute
+	lastSeenTTLSeconds = 180 // 3 minutes
 
 	// lastSeenCapacity specifies the max number of values that can be stored in
 	// the ttl map.
@@ -51,14 +51,14 @@ const (
 
 	// timelineInitTimeout specifies the amount of time to wait for the
 	// timeline to initialize.
-	timelineInitTimeout = 5 * time.Second
+	timelineInitTimeout = time.Minute
 
 	// updateTimelineTimeout specifies the amount of time to wait for events
 	// to be stored into the timeline.
 	updateTimelineTimeout = 5 * time.Second
 
-	// statusUpdateTimeout is the amount of time to wait between status update collections.
-	statusUpdateTimeout = 30 * time.Second
+	// StatusUpdateTimeout is the amount of time to wait between status update collections.
+	StatusUpdateTimeout = 30 * time.Second
 
 	// recycleTimeout is the amount of time to wait between recycle attempts.
 	// Recycle is a request to clean up / remove stale data that backends can choose to
