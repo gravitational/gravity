dfdd598910179e9055b5cb18c53fcbdf90b302e6
Roman Tkachenko <roman@gravitational.com>
2020-04-15 16:39:25 -0700

    Call status hook after install/upgrade. (#1385)

--- lib/install/phases/app.go
+++ lib/install/phases/app.go
@@ -36,7 +36,10 @@ import (
 
 // NewApp returns executor that runs install and post-install hooks
 func NewApp(p fsm.ExecutorParams, operator ops.Operator, apps app.Applications) (*hookExecutor, error) {
-	return NewHook(p, operator, apps, schema.HookInstall, schema.HookInstalled)
+	return NewHook(p, operator, apps,
+		schema.HookInstall,
+		schema.HookInstalled,
+		schema.HookStatus)
 }
 
 // NewHook returns executor that runs specified application hooks
