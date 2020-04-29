b2de0f0740dfa46342eae72d3977e959c398fd3e
Bernard Kim <bernard@gravitational.com>
2020-04-28 17:50:29 -0700

    [6.1.x] Disable nethealth check (#1465)
    * Bump planet to 6.1.22 (gravitational/planet#624)
    
    - Disables nethealth check 
    
    * Update robotest upgrade scenarios

--- build.assets/robotest_run_suite.sh
+++ build.assets/robotest_run_suite.sh
@@ -7,10 +7,10 @@ readonly UPGRADE_FROM_DIR=${1:-$(pwd)/../upgrade_from}
 declare -A UPGRADE_MAP
 
 # latest patch release on this branch, keep this up to date
-UPGRADE_MAP[6.1.18]="ubuntu:16"
+UPGRADE_MAP[6.1.24]="ubuntu:16"
 
 # latest patch release on previous compatible LTS, keep this up to date
-UPGRADE_MAP[5.5.37]="ubuntu:16"
+UPGRADE_MAP[5.5.41]="ubuntu:16"
 
 # first release from this release & last LTS, these don't need to change until a new major/minor release
 UPGRADE_MAP[6.1.0]="ubuntu:16"
@@ -18,10 +18,10 @@ UPGRADE_MAP[5.5.0]="ubuntu:16"
 
 # important versions in the field, per:
 # https://github.com/gravitational/robotest/issues/155#issuecomment-589743687
-UPGRADE_MAP[5.5.19]="ubuntu:16"
-UPGRADE_MAP[5.5.20]="ubuntu:16"
 UPGRADE_MAP[5.5.28]="ubuntu:16"
 UPGRADE_MAP[5.5.36]="ubuntu:16"
+UPGRADE_MAP[5.5.38]="ubuntu:16"
+UPGRADE_MAP[5.5.40]="ubuntu:16"
 
 readonly GET_GRAVITATIONAL_IO_APIKEY=${GET_GRAVITATIONAL_IO_APIKEY:?API key for distribution Ops Center required}
 readonly GRAVITY_BUILDDIR=${GRAVITY_BUILDDIR:?Set GRAVITY_BUILDDIR to the build directory}
