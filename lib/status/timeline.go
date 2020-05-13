d5a91ffc8bae2a887d9d40c94bbef5a40e5a440f
Bernard Kim <bernard@gravitational.com>
2020-05-13 09:45:46 -0700

    Collect status history in gravity report (#1520)
    * Add status history to cluster report
    
    * Dump entire unknown event struct
    
    * Edit leader elected formatting

--- lib/status/timeline.go
+++ lib/status/timeline.go
@@ -18,9 +18,13 @@ package status
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"time"
 
 	"github.com/gravitational/gravity/lib/httplib"
 
+	"github.com/fatih/color"
 	pb "github.com/gravitational/satellite/agent/proto/agentpb"
 	"github.com/gravitational/trace"
 	log "github.com/sirupsen/logrus"
@@ -38,4 +42,49 @@ func Timeline(ctx context.Context) (*pb.TimelineResponse, error) {
 		}
 	}()
 	return client.Timeline(ctx, &pb.TimelineRequest{})
+}
+
+// PrintEvent prints the event to the provided writer.
+func PrintEvent(w io.Writer, event *pb.TimelineEvent) {
+	timestamp := event.GetTimestamp().ToTime().Format(time.RFC3339)
+	switch event.GetData().(type) {
+	case *pb.TimelineEvent_ClusterDegraded:
+		fmt.Fprintln(w, color.RedString("%s [Cluster Degraded]",
+			timestamp))
+	case *pb.TimelineEvent_ClusterHealthy:
+		fmt.Fprintln(w, color.GreenString("%s [Cluster Healthy]",
+			timestamp))
+	case *pb.TimelineEvent_NodeAdded:
+		fmt.Fprintln(w, color.YellowString("%s [Node Added]\tnode=%s",
+			timestamp, event.GetNodeAdded().GetNode()))
+	case *pb.TimelineEvent_NodeRemoved:
+		fmt.Fprintln(w, color.YellowString("%s [Node Removed]\tnode=%s",
+			timestamp, event.GetNodeRemoved().GetNode()))
+	case *pb.TimelineEvent_NodeDegraded:
+		fmt.Fprintln(w, color.RedString("%s [Node Degraded]\tnode=%s",
+			timestamp, event.GetNodeDegraded().GetNode()))
+	case *pb.TimelineEvent_NodeHealthy:
+		fmt.Fprintln(w, color.GreenString("%s [Node Healthy]\tnode=%s",
+			timestamp, event.GetNodeHealthy().GetNode()))
+	case *pb.TimelineEvent_ProbeFailed:
+		e := event.GetProbeFailed()
+		fmt.Fprintln(w, color.RedString("%s [Probe Failed]\tnode=%s\tchecker=%s",
+			timestamp, e.GetNode(), e.GetProbe()))
+	case *pb.TimelineEvent_ProbeSucceeded:
+		e := event.GetProbeSucceeded()
+		fmt.Fprintln(w, color.GreenString("%s [Probe Succeeded]\tnode=%s\tchecker=%s",
+			timestamp, e.GetNode(), e.GetProbe()))
+	case *pb.TimelineEvent_LeaderElected:
+		e := event.GetLeaderElected()
+		if e.GetPrev() == "" {
+			fmt.Fprintln(w, color.YellowString("%s [Leader Elected]\tnew leader %s",
+				timestamp, e.GetNew()))
+		} else {
+			fmt.Fprintln(w, color.YellowString("%s [Leader Elected]\tleader changed %s -> %s",
+				timestamp, e.GetPrev(), e.GetNew()))
+		}
+	default:
+		fmt.Fprintln(w, color.YellowString("%s [Unknown Event]\t%s", timestamp, event))
+		log.WithField("event", event).Warn("Received unknown event type.")
+	}
 }
