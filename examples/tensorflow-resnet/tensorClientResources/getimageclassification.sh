ac5349422032652030783bef368fe780ea10d002
Ubuntu <ubuntu@gravsam8-cluster-node-2.c.kubeadm-167321.internal>
2020-06-14 20:17:00 +0000

    get image classification

---
+++ examples/tensorflow-resnet/tensorClientResources/getimageclassification.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+IMAGE_ID=$1
+errorValue=0
+
+if ! hash jq 2>/dev/null
+then
+    echo "'jq' was not found in PATH"
+    echo "jq is needed to process the image id"
+    errorValue=1
+fi
+
+
+if [[ "$#" -lt 1 ]]; then
+   echo 
+   echo "usage: getimageclassification.sh <imageclassifyids..222 52>"
+   echo "set RES_CLASSIFICATION to the classification json file. Default ./class.json"
+   errorValue=1
+fi
+
+if [ -z "$RES_CLASSIFICATION" ]; then
+   RES_CLASSIFICATION="./class.json"
+fi
+
+if [ ! -f "$RES_CLASSIFICATION" ]; then
+   echo "Classification file: $RES_CLASSIFICATION not available"
+   errorValue=1
+fi
+
+if [ "$errorValue" -eq "1" ]; then
+   exit 0 
+fi
+
+
+for var in "$@"
+do
+imageId=$(($var-1))
+echo "Image id checking: $imageId"
+classificationName=$( echo "cat $RES_CLASSIFICATION | jq -c '.[\"$imageId\"] | .[1] '" | bash | sed "s,\",,g" )
+echo "Classification name: $classificationName"
+ 
+done
+
+
+
+
+
+
+
+
+
