89886b38500e945dbe887889267a67c422104c89
Walt <walt@gravitational.com>
2020-06-25 19:50:35 -0700

    Update robotest upgrade targets. (#1760)
    * Update robotest upgrade targets.
    
    This patch drops validation of 6.1 to 7.1.0-alpha upgrades.
    These are replaced with validation from 7.0. Furthermore 6.2 and 6.3
    are included per our <=2 minor kubernetes version skew policy and some
    recent discussion with @knisbet about whether we support non-LTS to
    non-LTS upgrades.
    
    Quoting @knisbet:
    
    "6.2 has been out of support for more than 6 months at this point,
    and 6.3 hasn't had much adoption, with enterprise customers going to
    7.0, so it might be safe to try and save a few CPU cycles, dropping
    6.2 and maybe 6.3 for the time being."
    
    Thus I'm removing 6.2 and 6.3 from PR builds and keeping 6.2 and 6.3 in the nightlies.

--- build.assets/robotest_run_nightly.sh
+++ build.assets/robotest_run_nightly.sh
@@ -9,15 +9,17 @@ DOCKER_STORAGE_DRIVERS="overlay2"
 declare -A UPGRADE_MAP
 
 # latest patch release on compatible LTS, keep this up to date
-UPGRADE_MAP[6.1.18]="centos:7 debian:9 ubuntu:18"
+UPGRADE_MAP[7.0.10]="centos:7 debian:9 ubuntu:18"
 
-# latest patch release on supported non-LTS version, keep this up to date
-UPGRADE_MAP[6.3.6]="centos:7 debian:9 ubuntu:18"
+# latest patch release on compatible non-LTS versions
+UPGRADE_MAP[6.3.18]="centos:7 debian:9 ubuntu:18"
+UPGRADE_MAP[6.2.5]="centos:7 debian:9 ubuntu:18"
 
 # important versions in the field, these are static
-UPGRADE_MAP[6.1.0]="ubuntu:16"
-UPGRADE_MAP[6.2.5]="ubuntu:16"
+UPGRADE_MAP[7.0.0]="ubuntu:16"
 # UPGRADE_MAP[6.3.0]="ubuntu:16"  # disabled due to https://github.com/gravitational/gravity/issues/1009
+# UPGRADE_MAP[6.2.0]="ubuntu:16"  # pretty close to 6.2.5, no need to test both
+
 
 readonly GET_GRAVITATIONAL_IO_APIKEY=${GET_GRAVITATIONAL_IO_APIKEY:?API key for distribution Ops Center required}
 readonly GRAVITY_BUILDDIR=${GRAVITY_BUILDDIR:?Set GRAVITY_BUILDDIR to the build directory}
