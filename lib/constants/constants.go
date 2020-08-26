4a5a7e60194ab716e14e0e5bb2add5b99ecd283f
Roman Tkachenko <roman@gravitational.com>
2020-08-26 15:28:22 -0700

    Add check for http(-s) proxy format (#2049)

--- lib/constants/constants.go
+++ lib/constants/constants.go
@@ -217,6 +217,17 @@ const (
 	// GravityEnvVarPrefix is the prefix for gravity-specific environment variables.
 	GravityEnvVarPrefix = "GRAVITY_"
 
+	// HTTPProxyEnvVar is the HTTP_PROXY environment variable.
+	HTTPProxyEnvVar = "HTTP_PROXY"
+	// HTTPSProxyEnvVar is the HTTPS_PROXY environment variable.
+	HTTPSProxyEnvVar = "HTTPS_PROXY"
+
+	// DockerRegistry is a default name for private docker registry
+	DockerRegistry = "leader.telekube.local:5000"
+
+	// LocalRegistryAddr is the address of the local docker registry
+	LocalRegistryAddr = "127.0.0.1:5000"
+
 	// Localhost is local host
 	Localhost = "127.0.0.1"
 
