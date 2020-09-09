31a113c10ffb356bbf45ae48513da115fea5e67b
dmitri <deemok@gmail.com>
2020-09-09 13:00:12 +0200

    Backport https://github.com/gravitational/gravity/pull/2096

--- lib/rpc/server/agent.go
+++ lib/rpc/server/agent.go
@@ -174,15 +174,6 @@ func (srv *agentServer) Abort(ctx context.Context, req *types.Empty) (resp *type
 }
 
 func (srv *agentServer) command(req pb.CommandArgs, stream pb.Agent_CommandServer, log *log.Entry) (err error) {
-	defer func() {
-		r := recover()
-		if r == nil {
-			return
-		}
-
-		err = trace.BadParameter("panic for command %+v: %v", req, r)
-	}()
-
 	err = srv.commandExecutor.exec(stream.Context(), stream, req.Args, makeRemoteLogger(stream, srv.FieldLogger))
 	if err != nil {
 		log.WithError(err).Warn("Command completed with error.")
