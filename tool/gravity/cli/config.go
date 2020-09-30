5c09e27822609df3d6bb2d2a7483f664a96d8ab2
a-palchikov <deemok@gmail.com>
2020-09-30 20:40:58 +0200

    Take mount overrides into account in preflight checks. (#2170)

--- tool/gravity/cli/config.go
+++ tool/gravity/cli/config.go
@@ -531,6 +531,7 @@ func (i *InstallConfig) RunLocalChecks() error {
 		Manifest: app.Manifest,
 		Role:     role,
 		Docker:   i.Docker,
+		Mounts:   i.Mounts,
 		Options: &validationpb.ValidateOptions{
 			VxlanPort: int32(i.VxlanPort),
 			DnsAddrs:  i.DNSConfig.Addrs,
