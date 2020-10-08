05331241080f47f70b8c6dd9adb002a76114b320
Lenko <lenko@gravitational.com>
2020-10-08 12:35:27 -0500

    Added more servers to the test case of the findServer function.

--- tool/gravity/cli/utils_test.go
+++ tool/gravity/cli/utils_test.go
@@ -31,31 +31,46 @@ type UtilsSuite struct{}
 var _ = check.Suite(&UtilsSuite{})
 
 func (s *UtilsSuite) TestFindServer(c *check.C) {
-	server := storage.Server{
+	server1 := storage.Server{
 		AdvertiseIP: "10.0.0.1",
-		Hostname:    "hostName",
-		Nodename:    "nodeName",
-		InstanceID:  "i-sdfkwerjal",
+		Hostname:    "hostName1",
+		Nodename:    "nodeName1",
+		InstanceID:  "i-sdfkwerjal_1",
 	}
-	servers := []storage.Server{server}
 
-	tokens := []string{server.AdvertiseIP}
+	server2 := storage.Server{
+		AdvertiseIP: "10.0.0.2",
+		Hostname:    "hostName2",
+		Nodename:    "nodeName2",
+		InstanceID:  "i-sdfkwerjal_2",
+	}
+
+	server3 := storage.Server{
+		AdvertiseIP: "10.0.0.3",
+		Hostname:    "hostName3",
+		Nodename:    "nodeName3",
+		InstanceID:  "i-sdfkwerjal_3",
+	}
+
+	servers := []storage.Server{server1, server2, server3}
+
+	tokens := []string{server1.AdvertiseIP}
 	out, err := findServer(servers, tokens)
 	c.Assert(err, check.IsNil)
-	c.Assert(out.AdvertiseIP, check.Equals, server.AdvertiseIP)
+	c.Assert(out.AdvertiseIP, check.Equals, server1.AdvertiseIP)
 
-	tokens = []string{server.Hostname}
+	tokens = []string{server1.Hostname}
 	out, err = findServer(servers, tokens)
 	c.Assert(err, check.IsNil)
-	c.Assert(out.Hostname, check.Equals, server.Hostname)
+	c.Assert(out.Hostname, check.Equals, server1.Hostname)
 
-	tokens = []string{server.Nodename}
+	tokens = []string{server1.Nodename}
 	out, err = findServer(servers, tokens)
 	c.Assert(err, check.IsNil)
-	c.Assert(out.Nodename, check.Equals, server.Nodename)
+	c.Assert(out.Nodename, check.Equals, server1.Nodename)
 
-	tokens = []string{server.InstanceID}
+	tokens = []string{server1.InstanceID}
 	out, err = findServer(servers, tokens)
 	c.Assert(err, check.IsNil)
-	c.Assert(out.InstanceID, check.Equals, server.InstanceID)
+	c.Assert(out.InstanceID, check.Equals, server1.InstanceID)
 }
