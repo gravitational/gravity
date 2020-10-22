fdeebb9ec8966288c3a28bf8f74d996c9bf3eb4e
Lenko Donchev <1954225+lenko-d@users.noreply.github.com>
2020-10-22 15:38:34 -0500

    Certificate changes according to the latest MacOS specific requirements. (#2209)
    * Certificate changes according to the latest MacOS specific requirements.
    
    * Added comments describing MacOS specific requirements.
    
    * Use Teleport's TLSCredentials struct instead of copy-paste.
    
    * Test case for MacOS specific certificate properties.
    
    * Implemented auto-rotation of self signed cluster cert in order to mitigate the impact of the reduced validity period. The validity period was reduced due to MacOS requirements.
    
    * Better comment.
    
    * Use OrganizationUnit field to identify gravitational self signed cert.
    
    * Improved comment.
    
    * Use embedded error value instead of trace.DebugReport.
    
    * Removed duplicated log message.
    
    * Check for empty cert block after pem.Decode.
    
    * Extra info in the logs for the new cert(ExpirationDate and SerialNumber).
    
    * Debug log level for skip customer cert rotation message.
    
    * Extracted a constant to defaults.go.
    
    * Check if existing certificate files have already expired. This may happen if a user has been using their own cert for some time and have deleted it and decided to use the self-signed one.
    
    * Move SelfSignedCert organisation ID to lib/defaults.
    
    * Retry with backoff if the cert rotation fails.
    
    * 3 second initial interval for the backoff logic.
    
    * Certificate backdating. Letsencrypt backdates by 1 hour.
    
    * Dont do DNS lookups for localhost. Need to be careful of a very particular oddity of golang and some base images missing /etc/nsswitch.conf. Several versions of golang when missing nsswitch configuration, will use DNS without local host resolution. This means this lookup could query external DNS servers and be subject to a number of unexpected results.
    
    * A new name for self-signed cert org: Gravitiational Self Signed Web.
    
    * Improved comment.
    
    * Fixed spelling Organisation -> Organization.
    
    * Assert that the result block is not nil when doing pem.Decode.
    
    * Additional info in the log messages (cluster web UI cert).
    
    * Check for expired cluster web UI cert on process start.
    
    * Changed Lets Encrypt to Let's Encrypt for consistency.
    
    * Check for exact match when comparing cert OrganizationalUnit values.
    
    * Improved log messages.
    
    * Improved log message.
    
    * Improved log message.

--- lib/process/process.go
+++ lib/process/process.go
@@ -1488,6 +1488,7 @@ func (p *Process) initService(ctx context.Context) (err error) {
 		}
 
 		p.startService(p.runCertificateWatch(client))
+		p.startService(p.runCertExpirationWatch(client))
 		p.startService(p.runAuthGatewayWatch(client))
 		p.startService(p.runReloadEventsWatch(client))
 		p.startService(p.runRegistrySynchronizer)
@@ -1943,11 +1944,15 @@ func (p *Process) initClusterCertificate(ctx context.Context, client *kubernetes
 		return trace.Wrap(err)
 	}
 	if len(cert) != 0 && len(key) != 0 {
-		p.Info("Cluster certificate is already initialized.")
+		err = p.replaceCertIfAboutToExpire(ctx, p.client)
+		if err != nil {
+			return trace.Wrap(err)
+		}
+		p.Info("Cluster web UI certificate is already initialized.")
 		return nil
 	}
 
-	p.Info("Initializing cluster certificate.")
+	p.Infof("Initializing cluster web UI certificate from file: %v...", p.teleportConfig.Proxy.TLSCert)
 
 	certificateData, err := ioutil.ReadFile(p.teleportConfig.Proxy.TLSCert)
 	if err != nil {
@@ -1972,7 +1977,12 @@ func (p *Process) initClusterCertificate(ctx context.Context, client *kubernetes
 		return trace.Wrap(err)
 	}
 
-	p.Info("Cluster certificate has been initialized.")
+	err = p.replaceCertIfAboutToExpire(ctx, p.client)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	p.Infof("Successfully initialized cluster web UI certificate from file: %v.", p.teleportConfig.Proxy.TLSCert)
 	return nil
 }
 
@@ -2333,7 +2343,7 @@ func initSelfSignedHTTPSCert(cfg *service.Config, hostname string) (err error) {
 	if hostname != "" {
 		hosts = append(hosts, hostname)
 	}
-	creds, err := teleutils.GenerateSelfSignedCert(hosts)
+	creds, err := utils.GenerateSelfSignedCert(hosts)
 	if err != nil {
 		return trace.Wrap(err)
 	}
