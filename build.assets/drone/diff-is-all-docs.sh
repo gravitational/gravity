d3386265138e1b2df93f27ba484284df29b4b5d9
Walt Della <walt@gravitational.com>
2020-12-18 23:13:57 +0000

    Backport Drone CI
    This backports pr and release processes from master/8.0/7.0/6.1.
    
    (cherry picked from commit fd855bb95146906edf0d7b2109733e07e7c1203d)
    (cherry picked from commit b3f35f40c2db9e872a0c30477884c63dcb63ee1c)
    (cherry picked from commit 0bed3091a2885e6c34fcbcf507cd93acdc18d730)
    (cherry picked from commit 860d8c64276f217b2373180f3e12441bb7e3f037)
    (cherry picked from commit d89cb7a61a4258e40ebd7544ed79003975c01f9d)
    (cherry picked from commit 3cb2505cf2a5fc7f2bb23114e8d6c7c4b1193f3d)
    (cherry picked from commit 58653ed40d4dcd7bad5e2b43d98b175eb9fc7cb4)
    (cherry picked from commit 5b1425e70a29e2ca5605b54d5ed4a629566736b2)
    (cherry picked from commit d13b064ee038e996568f37b8eb95e7b8efd3591c)
    (cherry picked from commit 2f8b697350ac0ad825f3f89cbe50d5adcd436446)
    (cherry picked from commit f45ea659daa825fa2667f0be34d540cf1d25f287)
    (cherry picked from commit 207df3c8efe335b4fbdc041a12e58dc2b81317c7)
    (cherry picked from commit eb67766c1718736b289822359a9eec3516da3aed)
    (cherry picked from commit ea6c6db70334b4ca7025e8f73e103327dadd3c03)
    (cherry picked from commit 3e5e641b39637ae7b1dc5bd1056cb1a820c9e994)
    (cherry picked from commit bcf040ccdf022db09cf47a6083ed37c8a312196c)
    (cherry picked from commit 952b809357c27d0055d7336641645d21751b92dd)
    (cherry picked from commit 5e2b4c2e2bb1e1136a521e2c912060745f6b609a)
    (cherry picked from commit f12f19137c00956449e9207d6fa6ec5ae88c61dd)
    (cherry picked from commit a7b5f5af43451a0d67436c2a42f18af66cbf7502)

---
+++ build.assets/drone/diff-is-all-docs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Examine the output of git diff --raw to determine whether any files
+# which don't match the pattern '^docs/' or '.md$' were changed.
+
+REVLIST=$1
+if [ -z "$REVLIST" ]; then
+    echo "$0: please supply a git rev-list"
+    echo "For more info see: git help rev-list"
+    exit 2
+fi
+
+echo "---> git diff --raw ${REVLIST}"
+git diff --raw ${REVLIST}
+if [ $? -ne 0 ]; then
+    echo "---> Unable to determine diff"
+    exit 2
+fi
+git diff --raw ${REVLIST} | awk '{print $6}' | grep -Ev '^docs/' | grep -Ev '.md$' | grep -v ^$ | wc -l > /tmp/.change_count.txt
+export CHANGE_COUNT=$(cat /tmp/.change_count.txt | tr -d '\n')
+rm /tmp/.change_count.txt
+echo "---> Non-docs changes detected: $CHANGE_COUNT"
+if [ "$CHANGE_COUNT" -gt 0 ]; then
+    exit 1
+else
+    exit 0
+fi
