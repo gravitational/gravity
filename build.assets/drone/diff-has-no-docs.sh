58653ed40d4dcd7bad5e2b43d98b175eb9fc7cb4
Walt <walt@gravitational.com>
2021-02-24 10:39:54 -0800

    Drone CI: Automatically deploy docs upon merge to master (#2419)
    * Improve comments on short circuit logic.
    * Add documentation deploy to Drone CI.
    
    This will deploy all documentation deployed to master, replacing the
    Telekube-Deploy-Docs Jenkins job.

--- build.assets/drone/diff-has-no-docs.sh
+++ build.assets/drone/diff-has-no-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Examine the output of git diff --raw to determine whether any files
-# which don't match the pattern '^docs/' or '.md$' were changed.
+# which match the pattern '^docs/' were changed.
 
 REVLIST=$1
 if [ -z "$REVLIST" ]; then
@@ -15,10 +15,10 @@ if [ $? -ne 0 ]; then
     echo "---> Unable to determine diff"
     exit 2
 fi
-git diff --raw ${REVLIST} | awk '{print $6}' | grep -Ev '^docs/' | grep -Ev '.md$' | grep -v ^$ | wc -l > /tmp/.change_count.txt
+git diff --raw ${REVLIST} | awk '{print $6}' | grep -E '^docs/' | wc -l > /tmp/.change_count.txt
 export CHANGE_COUNT=$(cat /tmp/.change_count.txt | tr -d '\n')
 rm /tmp/.change_count.txt
-echo "---> Non-docs changes detected: $CHANGE_COUNT"
+echo "---> Docs changes detected: $CHANGE_COUNT"
 if [ "$CHANGE_COUNT" -gt 0 ]; then
     exit 1
 else
