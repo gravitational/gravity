b4dcd955a16b07f48a4c65090b89a3b37b48de7d
Sergei Antipov <s.antipov@mulesoft.com>
2021-03-30 08:51:57 -0400

    Forward-port chown of monitoring/logrange directories

--- lib/ops/opsservice/deploy.go
+++ lib/ops/opsservice/deploy.go
@@ -102,6 +102,10 @@ func remoteDirectories(operation ops.SiteOperation, server *ProvisionedServer, m
 		server.InGravity("secrets"),
 		server.InGravity("backup"),
 		server.InGravity("logrange"),
+		// names prometheus-db/alertmanager-db are hardcoded subPath values
+		// in prometheus-operator
+		server.InGravity("monitoring", "prometheus-db"),
+		server.InGravity("monitoring", "alertmanager-db"),
 	}
 
 	chownList := []string{
@@ -115,6 +119,8 @@ func remoteDirectories(operation ops.SiteOperation, server *ProvisionedServer, m
 		server.InGravity("site"),
 		server.InGravity("secrets"),
 		server.InGravity("backup"),
+		server.InGravity("monitoring"),
+		server.InGravity("logrange"),
 	}
 
 	chmodList := []string{
