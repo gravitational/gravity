2e7c6000329580afa0bf94ddc328a83e2418f26d
Walt Della <walt@gravitational.com>
2022-06-01 13:28:18 -0700

    Prune upgrade testing
    We no longer support 5.5 or 6.3. Best to prune these from our upgrade
    testing to speed up runtime as well as determinism.
    
    I also took this opportunity to prune some old 7.0 versions we're
    unlikely to see in the field.

--- assets/robotest/config/pr.sh
+++ assets/robotest/config/pr.sh
@@ -11,21 +11,8 @@ declare -A UPGRADE_MAP
 UPGRADE_MAP[$(recommended_upgrade_tag $(branch 7.0.x))]="redhat:8.4" # this branch
 UPGRADE_MAP[7.0.13]="centos:7.9" # 7.0.13 + centos is combination that is critical in the field -- 2020-07 walt
 UPGRADE_MAP[7.0.12]="ubuntu:18" # 7.0.12 is the first LTS 7.0 release
-UPGRADE_MAP[7.0.7]="ubuntu:16" # 7.0.7 is the first 7.0 with https://github.com/gravitational/planet/pull/671 included
-# UPGRADE_MAP[7.0.0]="ubuntu:16" # 7.0.0 is prone to upgrade failure without https://github.com/gravitational/planet/pull/671
 
 UPGRADE_MAP[$(recommended_upgrade_tag $(branch 6.1.x))]="redhat:7.9" # compatible LTS version
-# UPGRADE_MAP[6.1.0]="debian:9" # 6.1.0 hook init containers are broken (#2243)
-UPGRADE_MAP[$(recommended_upgrade_tag $(branch 6.3.x))]="debian:9" # compatible non-LTS version
-
-# INTERMEDIATE_UPGRADE_VERSION is the middle hop to use with --upgrade-via
-INTERMEDIATE_UPGRADE_VERSION=$(recommended_upgrade_tag $(branch 6.1.x))
-# INTERMEDIATE_UPGRADE_MAP maps gravity version -> linux distros that will be upgraded to a combined
-# latest + INTERMEDIATE_UPGRADE_VERSION image.  The chosen version will exercise a double etcd upgrade.
-# For example: v3.3.11 to v3.3.22 to v3.4.9
-declare -A INTERMEDIATE_UPGRADE_MAP
-INTERMEDIATE_UPGRADE_MAP[$(recommended_upgrade_tag $(branch 5.5.x))]="centos:7.9"
-INTERMEDIATE_UPGRADE_MAP[5.5.10]="centos:7.9"
 
 function build_upgrade_suite {
   local size='"flavor":"three","nodes":3,"role":"node"'
@@ -34,20 +21,6 @@ function build_upgrade_suite {
   for release in ${!UPGRADE_MAP[@]}; do
     local from_tarball=$(tag_to_image $release)
     for os in ${UPGRADE_MAP[$release]}; do
-      suite+=$(build_upgrade_step $from_tarball $to_tarball $os $size)
-      suite+=' '
-    done
-  done
-  echo -n $suite
-}
-
-function build_intermediate_upgrade_suite {
-  local size='"flavor":"three","nodes":3,"role":"node"'
-  local to_tarball=${INTERMEDIATE_INSTALLER_URL}
-  local suite=''
-  for release in ${!INTERMEDIATE_UPGRADE_MAP[@]}; do
-    local from_tarball=$(tag_to_image $release)
-    for os in ${INTERMEDIATE_UPGRADE_MAP[$release]}; do
       suite+=$(build_upgrade_step $from_tarball $to_tarball $os $size)
       suite+=' '
     done
@@ -97,10 +70,7 @@ EOF
 
 if [[ ${1} == "upgradeversions" ]] ; then
     UPGRADE_VERSIONS=${!UPGRADE_MAP[@]}
-    UPGRADE_VERSIONS+=" ${!INTERMEDIATE_UPGRADE_MAP[@]}"
     echo "$UPGRADE_VERSIONS"
-elif [[ ${1} == "intermediateversion" ]] ; then
-    echo "$INTERMEDIATE_UPGRADE_VERSION"
 elif [[ ${1} == "configuration" ]] ; then
     SUITE=""
     SUITE+=" $(build_telekube_suite)"
@@ -108,7 +78,6 @@ elif [[ ${1} == "configuration" ]] ; then
     SUITE+=" $(build_install_suite)"
     SUITE+=" $(build_resize_suite)"
     SUITE+=" $(build_upgrade_suite)"
-    SUITE+=" $(build_intermediate_upgrade_suite)"
     echo "$SUITE"
 else
     echo "Unknown parameter: $1"
