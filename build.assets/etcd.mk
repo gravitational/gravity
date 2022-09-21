a830bb0e4a9813901d8d751ad6762e46ddad9ff6
Walt <walt@goteleport.com>
2022-09-21 14:40:53 -0700

    Rotate test etcd CA and certs (#2750)
    These expired a month ago or so and all test are failing.
    
    I improved the Makefile to make rotating these certificates easier in
    the future.

--- build.assets/etcd.mk
+++ build.assets/etcd.mk
@@ -1,6 +1,6 @@
 where-am-i = $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
 TEST_ETCD_CERTS := $(realpath $(dir $(call where-am-i))../assets/certs)
-TEST_ETCD_CONFIG := '{"nodes": ["https://localhost:4001"], "key":"/gravity/test", "tls_key_file": "$(TEST_ETCD_CERTS)/proxy1-key.pem", "tls_cert_file": "$(TEST_ETCD_CERTS)/proxy1.pem", "tls_ca_file": "$(TEST_ETCD_CERTS)/ca.pem"}'
+TEST_ETCD_CONFIG := '{"nodes": ["https://localhost:4001"], "key":"/gravity/test", "tls_key_file": "$(TEST_ETCD_CERTS)/proxy1.key", "tls_cert_file": "$(TEST_ETCD_CERTS)/proxy1.crt", "tls_ca_file": "$(TEST_ETCD_CERTS)/ca.crt"}'
 TEST_ETCD_IMAGE := quay.io/coreos/etcd:v$(ETCD_VER)
 TEST_ETCD_INSTANCE := testetcd0
 
@@ -20,7 +20,7 @@ base-etcd:
 	  if [ "$$etcd_instance" != "" ]; then \
 	    docker rm -v $$etcd_instance; \
 	  fi; \
-	  docker run --net=host $(TEST_ETCD_MOUNTS) --name=$(TEST_ETCD_INSTANCE) -d -v $(TEST_ETCD_CERTS):/certs $(TEST_ETCD_IMAGE)  -name etcd0 -advertise-client-urls https://localhost:2379,https://localhost:4001  -listen-client-urls https://0.0.0.0:2379,https://0.0.0.0:4001  -initial-advertise-peer-urls https://localhost:2380  -listen-peer-urls https://0.0.0.0:2380  -initial-cluster-token etcd-cluster-1  -initial-cluster etcd0=https://localhost:2380  -initial-cluster-state new --cert-file=/certs/etcd1.pem --key-file=/certs/etcd1-key.pem --peer-cert-file=/certs/etcd1.pem --peer-key-file=/certs/etcd1-key.pem --peer-client-cert-auth --peer-trusted-ca-file=/certs/ca.pem -client-cert-auth --trusted-ca-file=/certs/ca.pem $(TEST_ETCD_FLAGS) ; \
+	  docker run --net=host $(TEST_ETCD_MOUNTS) --name=$(TEST_ETCD_INSTANCE) -d -v $(TEST_ETCD_CERTS):/certs $(TEST_ETCD_IMAGE)  -name etcd0 -advertise-client-urls https://localhost:2379,https://localhost:4001  -listen-client-urls https://0.0.0.0:2379,https://0.0.0.0:4001  -initial-advertise-peer-urls https://localhost:2380  -listen-peer-urls https://0.0.0.0:2380  -initial-cluster-token etcd-cluster-1  -initial-cluster etcd0=https://localhost:2380  -initial-cluster-state new --cert-file=/certs/etcd1.crt --key-file=/certs/etcd1.key --peer-cert-file=/certs/etcd1.crt --peer-key-file=/certs/etcd1.key --peer-client-cert-auth --peer-trusted-ca-file=/certs/ca.crt -client-cert-auth --trusted-ca-file=/certs/ca.crt $(TEST_ETCD_FLAGS) ; \
 	fi;
 
 .PHONY: etcd
